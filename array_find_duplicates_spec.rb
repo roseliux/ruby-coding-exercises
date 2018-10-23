851f4472edd1fc52abc5dd629be75cadf1dd7cd0
roseliux <roxdurazo@gmail.com>
2018-10-23 15:41:13 -0700

    Add solution for find duplicates on array.

---
+++ array_find_duplicates_spec.rb
@@ -0,0 +1,11 @@
+def duplicates?(array)
+  array.uniq.size != array.size
+end
+
+describe 'duplicates?' do
+  it 'returns boolean based on if an array has duplicates values' do
+    expect( duplicates?([]) ).to eq(false)
+    expect( duplicates?(Array(1..5)) ).to eq(false)
+    expect( duplicates?([1, 2, 3, 4, 5, 1]) ).to eq(true)
+  end
+end
